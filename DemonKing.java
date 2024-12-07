448ff0cd4f12c626f74a2d30dd687f728ed5d8fe
Blank-Vulture <166619679+Blank-Vulture@users.noreply.github.com>
2024-12-07 19:33:29 +0900

    授業修了後整理

--- DemonKing.java
+++
@@ -1,7 +0,0 @@
-public class DemonKing extends Fighter {
-    private int baseAttackPower;
-
-    public DemonKing(String name, int maxHp, int baseAttackPower) {
-        super(name, maxHp);
-        this.baseAttackPower = baseAttackPower;
-    }
